dc803d855066ee8bb861a9c25a841c2825ad5dd7
Nate <nate@interactivebalance.com>
2013-04-28 15:24:55 -0700

    ddd

--- public/js/mediators/home.js
+++ public/js/mediators/home.js
@@ -2,6 +2,17 @@
 $(document).ready(function() {
   // Handler for .ready() called.
 
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-7765366-7']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+
   var dNum = .3;
   for(i = 0; i < $('.widePost').length; i ++){
     TweenLite.from($('.widePost')[i], .3, {delay:dNum, css:{opacity:0, y:20}});
