05af47e9f8aafdcfbdb0fe7d54370d42624df993
Nate <nate@interactivebalance.com>
2013-10-18 11:15:57 -0700

    foundation added

--- ibalance.js
+++ ibalance.js
@@ -12,7 +12,7 @@ var express = require('express')
 var app = express();
 
 // all environments
-app.set('port', process.env.PORT || 4000);
+app.set('port', process.env.PORT || 3000);
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon(__dirname + '/public/img/favicon.ico', { maxAge: 2592000000 }));
